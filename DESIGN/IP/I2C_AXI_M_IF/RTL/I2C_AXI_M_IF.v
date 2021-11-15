fd287910e50e40fe953598a0e24c6b21ddb2a3d0
Kelvinlee501 <dlwhdxkr56@gmail.com>
2021-11-15 13:41:32 +0900

    update env temp 211115

---
+++ DESIGN/IP/I2C_AXI_M_IF/RTL/I2C_AXI_M_IF.v
@@ -0,0 +1,14 @@
+////////////////////////////////////
+//
+//
+//
+////////////////////////////////////
+
+module I2C_AXI_M_IF #(
+	parameter			PARAM = 1	
+) (
+	input						I_CLK,
+	input						I_RESETn
+);
+
+endmodule
---
+++ DESIGN/IP/I2C_AXI_M_IF/RTL/vcode.f
@@ -0,0 +1,1 @@
+/home/kelvinlee/SoCstudy/DESIGN/IP/I2C_AXI_M_IF/RTL/I2C_AXI_M_IF.v
