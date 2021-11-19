4a9d377e52b192586bd010f719aed67bfca43183
Kelvinlee501 <dlwhdxkr56@gmail.com>
2021-11-19 15:09:19 +0900

    Take a snapshot for slave normal test seq 211119

--- DESIGN/IP/I2C/SIM/tests/test_slave_normal.sv
+++ DESIGN/IP/I2C/SIM/tests/test_slave_normal.sv
@@ -17,39 +17,53 @@ module test_slave_normal;
 		apb_model.apb4m_config;
 
         TASK_CONFIG_SLAVE_MODE;
-        TASK_SINGLE_TRANSACTION_SLAVE;
+        TASK_ONE_BYTE_TRANSACTION_SLAVE(8'hab);
     endtask
 
     task TASK_CONFIG_SLAVE_MODE;
        $display("[INFO] Set I2C slave mode "); 
 
        //set sfr with apb4 interface to change slave mode of i2c.
+       //'Julian' will describe this task more detail.
        //default setting is i2c master in sfr
        task_sfr_config(`SLAVE_MODE_ADDR,32'h0000_0001);
+       //I2C speed mode selection
+       // 0 : legacy speed mode
+       // 1 : fast speed mode
+       // 2 : high speed mode
+       task_sft_config(`DATA_RATE_CONF_ADDR,32'h0000_0000);
+
+       //AXI mode selection
+       //!!!!!this mode need to be dicussed for designing I2C IP.
+       // 0 : APB
+       // 1 : AXI
+       task_sft_config(`AXI_MODE_SEL_ADDR,32'h0000_0000);
 
        // Config i2c vip mode to master mode
        // I2C IF DV owner 'William' will describe this task below.
        task_i2c_vip_config(`MASTER_MODE);
     endtask
 
-    task TASK_SINGLE_TRANSACTION_SLAVE;
+    task TASK_ONE_BYTE_TRANSACTION_SLAVE;
+       input [7:0] w_data;
         
-       reg [7:0] data;
+       reg [31:0] data;
        $display("[INFO] Single transaction start for testing slave mode "); 
 
        // I2C vip master write single write transaction.
        // 'William' will describe this task below.
-       task_i2c_vip_m_single_write(`DEVICE_ADDR,8'b1010_1010);
+       task_i2c_vip_m_one_byte_write(`DEVICE_ADDR,w_data);
         
        //It will be wrapped in task_apb.sv 
        top.apb4m_vip.task_apb_read(_result,`RX_DATA_ADDR,data);
 
-       if(data == 8'b1010_1010) begin
-            $display("\n TEST PASSED\n");
-       end
+       //temporarily compare point
+       if(data[7:0] == w_data) begin
+            $display("TEST PASSED");
        else begin
-            $display("\n TEST FAILED\n");
+            $display("TEST FAILED");
        end
+
     endtask
 
     task task_sfr_config;
