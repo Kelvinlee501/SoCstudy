7f02676847da2e366052f8a7edac415feae152dd
Kelvinlee501 <dlwhdxkr56@gmail.com>
2022-09-09 22:36:34 +0900

    test3

--- DESIGN/IP/I2C/RTL/I2C.v
+++ DESIGN/IP/I2C/RTL/I2C.v
@@ -9,6 +9,19 @@ module I2C #(
 ) (
 	input						I_CLK,
 	input						I_RESETn
+    
+    //AMBA4 APB interface
+    input PCLK,
+    input PRESETn,
+    input [31:0] PADDR,
+    input PWRTIE,
+    input PSEL,
+    input PENABLE,
+    input [31:0] PWDATA,
+    output reg [31:0] PRDATA,
+    output reg PREADY,
+    output reg PSLVERR
+
 );
 
 endmodule
