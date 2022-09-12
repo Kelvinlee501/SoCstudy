03e1d8a2c694ab1f3ac9dd19295b93849187b4a4
Kelvinlee501 <dlwhdxkr56@gmail.com>
2022-09-12 23:25:25 +0900

    SCC_8bit control logic update 20220912

--- DESIGN/IP/SCC_8bit/RTL/CONTROL_LOGIC.v
+++ DESIGN/IP/SCC_8bit/RTL/CONTROL_LOGIC.v
@@ -14,42 +14,83 @@ module CONTROL_LOGIC(
     //selection signals
     output reg pc_jump_sel,
     output reg pc_inc_sel,
-    output reg datamem_sel
+    output reg data_mem_sel
 
-    //instruction memory controls
+    //instruction memory control signals
+    output reg inst_mem_ld,
 
-    //data memory controls
-
+    //data memory control signals
+    output reg data_mem_wt
 
 );
 
-    //-----------------------------
+    //--------------------------------------------------------------
     // define local parameters
-    //-----------------------------
-    localparam IF  = 3'b000;
-    localparam ID  = 3'b001;
-    localparam EX  = 3'b010;
-    localparam MEM = 3'b011;
-    localparam WB  = 3'b100;
+    //--------------------------------------------------------------
+    //Instructions
+    localparam HALT  = 3'b000;
+    localparam SIFZ  = 3'b001;
+    localparam ADD   = 3'b010;
+    localparam AND   = 3'b011;
+    localparam XOR   = 3'b100;
+    localparam LOAD  = 3'b101;
+    localparam STORE = 3'b110;
+    localparam JUMP  = 3'b111;
+
+    //FSM states
+    localparam IDLE = 3'b000;
+    localparam IF   = 3'b001;
+    localparam ID   = 3'b010;
+    localparam EX   = 3'b011;
+    localparam MEM  = 3'b100;
+    localparam WB   = 3'b100;
     
-    //-----------------------------
+    //--------------------------------------------------------------
     // define reg type variables
-    //-----------------------------
+    //--------------------------------------------------------------
     reg [2:0] pstate;
     reg [2:0] nstate;
 
 
-    //-----------------------------
+    //--------------------------------------------------------------
     // Control Logic FSM
-    //-----------------------------
+    //--------------------------------------------------------------
     always@(posedge clk or negedge resetn) begin
-        if(~resetn) pstate <= IF;
+        if(~resetn) pstate <= IDLE;
         else        pstate <= nstate;
     end
 
     always@(*) begin
         case(pstate)
-            IF : ;
+            IDLE : begin
+                {pc_ld, flag_ld, acc_ld, mdr_ld}        = 4'b0000;
+                {pc_jump_sel, pc_inc_sel, data_mem_sel} = 3'b000;
+                {inst_mem_ld, data_mem_wt}              = 2'b00;
+                nstate = IF;
+            end
+            IF   : begin
+                {pc_ld, flag_ld, acc_ld, mdr_ld}        = 4'b0000;
+                {pc_jump_sel, pc_inc_sel, data_mem_sel} = 3'b000;
+                {inst_mem_ld, data_mem_wt}              = 2'b10;
+                nstate = ID;
+            end
+            ID   : begin
+                if(opcode == HALT) begin
+                    {pc_ld, flag_ld, acc_ld, mdr_ld}        = 4'b0000;
+                    {pc_jump_sel, pc_inc_sel, data_mem_sel} = 3'b000;
+                    {inst_mem_ld, data_mem_wt}              = 2'b00;
+                    nstate = IF;
+                end
+                else if(opcode == SIFZ) begin
+
+                end
+                else if(opcode == JUMP) begin
+                    {pc_ld, flag_ld, acc_ld, mdr_ld}        = 4'b1000;
+                    {pc_jump_sel, pc_inc_sel, data_mem_sel} = 3'b100;
+                    {inst_mem_ld, data_mem_wt}              = 2'b00;
+                    nstate = IF;
+                end
+            end
         endcase
     end
 
--- DESIGN/IP/SCC_8bit/RTL/FLAG.v
+++ DESIGN/IP/SCC_8bit/RTL/FLAG.v
@@ -16,3 +16,31 @@ module FLAG(
         end
     end
 endmodule
+
+module ACC(
+        input resetn,
+        input clk,
+        input ld,
+        input [7:0] acc_in,
+        output reg [7:0] acc_out
+);
+    
+    always@(posedge clk or negedge resetn) begin
+        if(~resetn) acc_out <= 8'b0000_0000;
+        else if(ld) acc_out <= acc_in;
+    end
+endmodule
+
+module MDR(
+        input resetn,
+        input clk,
+        input ld,
+        input [7:0] mdr_in,
+        output reg [7:0] mdr_out
+);
+    
+    always@(posedge clk or negedge resetn) begin
+        if(~resetn) mdr_out <= 8'b0000_0000;
+        else if(ld) mdr_out <= mdr_in;
+    end
+endmodule
